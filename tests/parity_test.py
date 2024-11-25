29d3175ede7827ffea620b4a0d74053ebfd66a74
hamolicious <hamolicious@hamolicious.net>
2024-11-25 21:27:46 +0000

    expand test

--- tests/parity_test.py
+++ tests/parity_test.py
@@ -39,22 +39,58 @@ def test_ls_parity() -> None:
 def test_ls_dir_parity() -> None:
     print()
     gh, gl = create_gh_gl(ref="dir-test")
-    files_gh: list[Directory] = [f for f in gh.ls() if isinstance(f, Directory)]
-    files_gl: list[Directory] = [f for f in gl.ls() if isinstance(f, Directory)]
 
+    # grab directories only from gh and gl
+    files_gh: list[Directory] = [
+        f for f in gh.ls() if isinstance(f, Directory) and not f.name.startswith(".")
+    ]
+    files_gl: list[Directory] = [
+        f for f in gl.ls() if isinstance(f, Directory) and not f.name.startswith(".")
+    ]
+    print(f"{files_gh=} {files_gl=}")
+
+    # convert those dirs to names
     fa1 = list(map(lambda f: f.name, files_gl))
     fb1 = list(map(lambda f: f.name, files_gh))
+    print(f"{fa1=} {fb1=}")
     assert fa1 == fb1
 
-    files_gh = gh.ls(cast(Directory, files_gh[0]).path)
-    files_gl = gl.ls(cast(Directory, files_gl[0]).path)
+    # look inside the first dir
+    files_gh = gh.ls(cast(Directory, files_gh[1]).path)
+    files_gl = gl.ls(cast(Directory, files_gl[1]).path)
+    print(f"{files_gh=} {files_gl=}")
 
+    # convert _those_ files into path names
     fa2 = list(map(lambda f: str(f.path), files_gl))
     fb2 = list(map(lambda f: str(f.path), files_gh))
+    print(f"{fa2=} {fb2=}")
+
+    # make sure that gh and gl files are the same
     assert fa2 == fb2
 
+    # make sure that files 1 level deep are different to files 2 levels deep
     assert fa1 != fa2
     assert fb1 != fb2
+
+    # go one level deeper
+    files_gh = [
+        f
+        for f in gh.ls(cast(Directory, files_gh[0]).path)
+        if isinstance(f, Directory) and not f.name.startswith(".")
+    ]
+    files_gl = [
+        f
+        for f in gl.ls(cast(Directory, files_gl[0]).path)
+        if isinstance(f, Directory) and not f.name.startswith(".")
+    ]
+
+    # convert those dirs to path names
+    fa = list(map(lambda f: str(f.path), files_gl))
+    fb = list(map(lambda f: str(f.path), files_gh))
+    print(f"{fa=} {fb=}")
+
+    # make sure that the files 3 levels deep are equal between gh and gl
+    assert fa == fb
 
 
 def test_file_parity() -> None:
